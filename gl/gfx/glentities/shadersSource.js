f555b948b4f526a0d2a61eef324ff2840315f125
DmIV3 <73702232+DmIV3@users.noreply.github.com>
2023-06-27 12:15:45 +0200

    Update shadersSource.js

--- gl/gfx/glentities/shadersSource.js
+++ gl/gfx/glentities/shadersSource.js
@@ -1,8 +1,7 @@
 const VSOURCE = 
 `   #version 300 es
     precision mediump float;
-
-
+    
     layout(location=0) in vec3 a_Pos;
     layout(location=1) in vec3 a_Normal;
     layout(location=2) in vec2 a_TexCoord;
@@ -26,7 +25,8 @@ const VSOURCE =
 const FSOURCE = 
 `   #version 300 es
     precision mediump float;
-
+    precision mediump sampler2D;
+    
     uniform sampler2D u_Sampler;
 
     in float v_Brightness;
