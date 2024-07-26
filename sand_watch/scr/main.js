17c0594bbeb27173793ed5db2ff8b9a1e103a70f
DmIV3 <73702232+DmIV3@users.noreply.github.com>
2024-07-26 10:06:09 +0200

    Update main.js

--- sand_watch/scr/main.js
+++ sand_watch/scr/main.js
@@ -1,29 +1,11 @@
 import { Watch } from "./entity.js";
 
-MOUSE.init();
-
-let prevTime = 0,
-    deltaTime = 0;
-
-
 const watch = Watch.create();
-let t = 0.05;
 
-function tick(elapsedTime=0){
-    deltaTime = (elapsedTime - prevTime) / 1000;
-    prevTime = elapsedTime;
 
-    t -= deltaTime;
-    if(t <= 0){
-        TWEEN.update(deltaTime);
-
-        watch.update();
-        watch.render();
-
-        t = 0.05 + t;
-        MOUSE.update();
-    }
-
-    requestAnimationFrame(tick)
+function tick(){
+    watch.update();
+    watch.render();
 }
-tick(0);
+
+setInterval(tick, 1000 / 30);
